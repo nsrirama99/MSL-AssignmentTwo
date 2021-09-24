e1ba754fd59451282078f715333b9f8dd1a739af
Nathan <nsrirama@smu.edu>
2021-09-24 17:45:56 -0500

    storyboard update + initial code

--- AssignmentTwo/ViewController.swift
+++ AssignmentTwo/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Freq2Label: UILabel!
+    @IBOutlet weak var Freq1Label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ AssignmentTwo/ViewControllerTwo.swift
@@ -0,0 +1,32 @@
+//
+//  ViewControllerTwo.swift
+//  AssignmentTwo
+//
+//  Created by UbiComp on 9/22/21.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+
+    @IBOutlet weak var FreqSlider: UISlider!
+    @IBOutlet weak var GestureLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
